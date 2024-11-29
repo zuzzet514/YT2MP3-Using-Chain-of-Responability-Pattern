21ef508798c8570316044abe10669c5e72c1a819
Zuzzet Suarez <zuzzet.hs14@gmail.com>
2024-11-29 16:32:40 -0600

    api 2

--- FetchMp3DataHandler.js
+++ FetchMp3DataHandler.js
@@ -3,6 +3,11 @@ const fetch = require("node-fetch");
 
 // https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}
 
+/*
+"x-rapidapi-key": process.env.API_KEY,
+"x-rapid-host": process.env.API_HOST,
+*/
+
 class FetchMp3DataHandler extends Handler{
 
 }
