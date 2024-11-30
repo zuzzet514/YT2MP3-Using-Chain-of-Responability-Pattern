e06eeb9aa5ed335503109b9ac16593dacd10c494
Hernandez Suarez Zuzzet <zS22017025@estudiantes.uv.mx>
2024-11-29 19:10:44 -0600

    fin exposicion

--- DownloadMp3Handler.js
+++ DownloadMp3Handler.js
@@ -6,6 +6,26 @@ const os = require("os");
 
 class DownloadMp3Handler extends Handler{
 
+    async handle(input){
+        const { title, link } = input;
+
+        try {
+            console.log(`Downloading mp3 for ${title}`)
+
+            const response = await fetch(link)
+            const buffer = await response.buffer();
+
+            const downloadDir = path.join(os.homedir(), "Downloads");
+            const filepath = path.join(downloadDir, `${title}.mp3`);
+
+            fs.writeFileSync(filepath, buffer);
+
+            console.log("download completed")
+
+        } catch (error) {
+            console.log("error downloading mp3 file", error.message)
+        }
+    }
 }
 
 module.exports = { DownloadMp3Handler };
--- FetchMp3DataHandler.js
+++ FetchMp3DataHandler.js
@@ -1,15 +1,46 @@
 const Handler = require("./Handler");
 const fetch = require("node-fetch");
 
-// https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}
+//
 
 /*
-"x-rapidapi-key": process.env.API_KEY,
-"x-rapid-host": process.env.API_HOST,
+
 */
 
 class FetchMp3DataHandler extends Handler{
 
+    async handle(videoId){
+        try {
+            const response = await fetch(
+                `https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "x-rapidapi-key": process.env.API_KEY,
+                        "x-rapid-host": process.env.API_HOST,
+                    },
+                }
+            );
+
+            const data = await response.json();
+            console.log(`API response:`, data)
+
+            if (data.status === "ok") {
+                console.log("fetching mp3 download link");
+
+                return super.handle({
+                    title: data.title,
+                    link: data.link,
+                })
+            }else if(data.msg === "in queue") {
+                console.warn("video is in queue try again later")
+            } else  {
+                console.error(`Error ${data.msg}`)
+            }
+        } catch (error) {
+            console.log("error fetching api data")
+        }
+    }
 }
 
 module.exports = { FetchMp3DataHandler };
